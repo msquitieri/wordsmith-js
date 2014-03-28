4b1fb70783e480b9aadf9219fcfb9c165d12d6a1
squit <michael.squitieri@gmail.com>
2014-03-28 02:11:26 -0400

    added method sentence_count

--- readability.js
+++ readability.js
@@ -23,5 +23,16 @@ var readability = {
 	word = word.replace(/(?:[^laeiouy]es|ed|[^laeiouy]e)$/, '');   
 	word = word.replace(/^y/, '');                                 
 	return word.match(/[aeiouy]{1,2}/g).length;     
+  },
+
+  sentence_count : function(string) {
+  	var realSentences = [];
+  	var possibleSentences = string.split(/[\.?!]/);
+  	for (var i = possibleSentences.length - 1; i >= 0; i--) {
+  		var sentence = possibleSentences[i].trim();
+  		if (sentence != "") realSentences.push(sentence);
+  	}
+  	return realSentences.length;
   }
+
 }
