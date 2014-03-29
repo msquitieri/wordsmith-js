67dba1d1a48e11c4f440969efd5ca33faff93182
squit <michael.squitieri@gmail.com>
2014-03-28 20:53:28 -0400

    fixed indentation

--- readability.js
+++ readability.js
@@ -18,11 +18,11 @@ var readability = {
   },
 
   word_syllable_count : function(word) {
-	word = word.toLowerCase();                                     
-	if (word.length <= 3) { return 1; }                            
-	word = word.replace(/(?:[^laeiouy]es|ed|[^laeiouy]e)$/, '');   
-	word = word.replace(/^y/, '');                                 
-	return word.match(/[aeiouy]{1,2}/g).length;     
+  	word = word.toLowerCase();                                     
+  	if (word.length <= 3) { return 1; }                            
+  	word = word.replace(/(?:[^laeiouy]es|ed|[^laeiouy]e)$/, '');   
+  	word = word.replace(/^y/, '');                                 
+  	return word.match(/[aeiouy]{1,2}/g).length;     
   },
 
   sentence_count : function(string) {
