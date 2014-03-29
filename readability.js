0bb03360b76233d2b4be6e319f529f29123de3d3
squit <michael.squitieri@gmail.com>
2014-03-29 01:35:48 -0400

    fixed indentation

--- readability.js
+++ readability.js
@@ -1,20 +1,20 @@
 var readability = {
   syllable_count : function(data) {
-  	if (typeof data != "string" && !(data instanceof Array)) return null;
-  	if (data instanceof Array) data = data.join(" ");
+    if (typeof data != "string" && !(data instanceof Array)) return null;
+    if (data instanceof Array) data = data.join(" ");
 
-  	console.log("passed");
+    console.log("passed");
 
-  	var allWords = data.split(" ");
-  	for (var i=0; i<allWords.length; i++) allWords[i] = allWords[i].trim();
+    var allWords = data.split(" ");
+    for (var i=0; i<allWords.length; i++) allWords[i] = allWords[i].trim();
 
-  	console.log(allWords);
+    console.log(allWords);
 
-  	var totalSyllableCount = 0;
-  	for (var i = allWords.length - 1; i >= 0; i--) {
-  		totalSyllableCount += this.word_syllable_count(allWords[i]);
-  	}
-  	return totalSyllableCount;
+    var totalSyllableCount = 0;
+    for (var i = allWords.length - 1; i >= 0; i--) {
+      totalSyllableCount += this.word_syllable_count(allWords[i]);
+    }
+    return totalSyllableCount;
   },
 
   remove_punctuation : function(string) {
@@ -36,22 +36,22 @@ var readability = {
   },
 
   word_syllable_count : function(word) {
-  	word = word.toLowerCase();                                     
-  	if (word.length <= 3) { return 1; }                            
-  	word = word.replace(/(?:[^laeiouy]es|ed|[^laeiouy]e)$/, '');   
-  	word = word.replace(/^y/, '');                                 
-  	return word.match(/[aeiouy]{1,2}/g).length;     
+    word = word.toLowerCase();                                     
+    if (word.length <= 3) { return 1; }                            
+    word = word.replace(/(?:[^laeiouy]es|ed|[^laeiouy]e)$/, '');   
+    word = word.replace(/^y/, '');                                 
+    return word.match(/[aeiouy]{1,2}/g).length;     
   },
 
   sentence_count : function(string) {
-  	var realSentences = [];
+    var realSentences = [];
     var sentenceDelimeter = /[\.?!]/;
-  	
+    
     var possibleSentences = string.split(sentenceDelimeter);
-  	for (var i = possibleSentences.length - 1; i >= 0; i--) {
-  		var sentence = possibleSentences[i].trim();
-  		if (sentence != "") realSentences.push(sentence);
-  	}
-  	return realSentences.length;
+    for (var i = possibleSentences.length - 1; i >= 0; i--) {
+      var sentence = possibleSentences[i].trim();
+      if (sentence != "") realSentences.push(sentence);
+    }
+    return realSentences.length;
   }
 }
