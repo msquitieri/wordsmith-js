8cb34f9e0cbcffd6bc75c76581d12d75ad9d46ad
squit <michael.squitieri@gmail.com>
2015-02-02 00:36:06 -0500

    remove trailing whitespace

--- readability.js
+++ readability.js
@@ -2,7 +2,7 @@ var readability = {
   syllable_count : function(data) {
     if (typeof data != "string" && !(data instanceof Array)) return null;
     if (data instanceof Array) data = data.join(" ");
-    
+
     var allWords = this.get_words(data);
 
     var totalSyllableCount = 0;
@@ -34,17 +34,17 @@ var readability = {
   },
 
   word_syllable_count : function(word) {
-    word = word.toLowerCase();                                     
-    if (word.length <= 3) { return 1; }                            
-    word = word.replace(/(?:[^laeiouy]es|ed|[^laeiouy]e)$/, '');   
-    word = word.replace(/^y/, '');                                 
-    return word.match(/[aeiouy]{1,2}/g).length;     
+    word = word.toLowerCase();
+    if (word.length <= 3) { return 1; }
+    word = word.replace(/(?:[^laeiouy]es|ed|[^laeiouy]e)$/, '');
+    word = word.replace(/^y/, '');
+    return word.match(/[aeiouy]{1,2}/g).length;
   },
 
   sentence_count : function(string) {
     var realSentences = [];
     var sentenceDelimeter = /[\.?!]/;
-    
+
     var possibleSentences = string.split(sentenceDelimeter);
     for (var i = possibleSentences.length - 1; i >= 0; i--) {
       var sentence = possibleSentences[i].trim();
@@ -58,7 +58,7 @@ var readability = {
     var sentenceCount = this.sentence_count(string);
     var syllableCount = this.syllable_count(string);
 
-    return ( 0.39*(wordCount/sentenceCount) + 11.8*(syllableCount/wordCount) - 15.59 ); 
+    return ( 0.39*(wordCount/sentenceCount) + 11.8*(syllableCount/wordCount) - 15.59 );
   },
 
   flesch_reading_ease_score : function(string) {
@@ -66,7 +66,6 @@ var readability = {
     var sentenceCount = this.sentence_count(string);
     var syllableCount = this.syllable_count(string);
 
-    return ( 206.835 - 1.015*(wordCount/sentenceCount) - 84.6*(syllableCount/wordCount) ); 
-
+    return ( 206.835 - 1.015*(wordCount/sentenceCount) - 84.6*(syllableCount/wordCount) );
   }
 }
